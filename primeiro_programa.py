c0d9e5834479df27829d2542b020879d7f30fed5
carlosedu60 <carlosedu60@hotmail.com>
2023-08-10 16:43:22 -0300

    acrescentando algumas revisões de python

--- primeiro_programa.py
+++ primeiro_programa.py
@@ -1,3 +1,25 @@
 #tela de boas vindas
 print("Hello World")
 print("Seja bem vindo ao curso de Python")
+print('')
+print('xxxxxxxxxxxxxxxxxxxxxxx')
+print('')
+
+ 
+#Estruturas condicionais
+MAIOR_IDADE = 18
+
+idade = int(input("Informe sua idade: "))
+    
+if idade >= MAIOR_IDADE:
+    print('Maior de idade, já pode tirar sua habilitação.')
+
+if idade < MAIOR_IDADE:
+    print('Menor de idade, Ainda não pode tirar sua habilitação.')
+    
+    
+if idade >= MAIOR_IDADE:
+    print('Já pode tirar sua habilitação.')
+else:
+    print('Ainda não pode tirar sua habilitação.')
+
